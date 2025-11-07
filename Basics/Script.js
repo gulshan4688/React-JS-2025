15f06d7c3c782afff7c8780397ad51045650d8fc
Gulshan.Kumar <gulshankumar4688@gmail.com>
2025-11-07 18:32:22 +0530

    notes.md

--- Basics/Script.js
+++ Basics/Script.js
@@ -1,5 +1,16 @@
-var h1 = document.createElement('h1')
+// import {a, arr} from './app.js'
+// console.log(a);
 
-h1.innerHTML = 'hello from inner html'
-console.log(h1);
-document.body.appendChild(h1);
+// arr.forEach(element => {
+//     console.log(element);
+// });
+
+// console.log(arr);
+
+import user from './app.js'    // default variables are imported this way  
+// here we can write any name and that works because its an default export 
+import {age, arr} from './app.js'   // export named are imported this way  in { BRACKETS }
+//  but here we have to be care full to the naming , they should be exactly matching 
+console.log(user);
+console.log(arr);
+console.log(age);
---
+++ Basics/app.js
@@ -0,0 +1,10 @@
+// export const a = 20
+// export const arr = [1,2,3,4];
+
+
+
+const user = "sarthak"
+export const age = 23;
+export const arr = ["who", "am", "I"];
+
+export default user;
