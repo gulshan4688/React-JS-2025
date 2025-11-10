36d69e6a7eda2765c76284a16557d29c4df03e4e
Gulshan.Kumar <gulshankumar4688@gmail.com>
2025-11-10 17:46:25 +0530

    modular css

---
+++ myapp4-CSS/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css'
+const Header = () => {
+    return (
+        <div className={styles.Header} >
+            <button className={styles.btn} > header wala button  </button>
+            <h3  >Gulshans</h3>
+        </div>
+    )
+}
+
+export default Header
