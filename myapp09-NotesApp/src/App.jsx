4061721d4a4f3ae465efe74856e242be510f4508
Gulshan.Kumar <gulshankumar4688@gmail.com>
2025-11-12 15:18:41 +0530

    learning notes making

--- myapp09-NotesApp/src/App.jsx
+++ myapp09-NotesApp/src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+  // const [title, setTitle] = useState('');
+  // const [description, setDescription] = useState('');
+  const [notes, setNotes] = useState([{}]);
+  const submitHandler=(e)=>{
+    e.preventDefault();
+
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen bg-gray-800 text-gray-300 ' >
+      <form className='flex p-10 flex-col gap-4' > 
+        <input
+          type="text"
+          placeholder='enter Heading'
+          className='px-5 py-2 border-2 rounded outline-none '
+          value={notes.title}
+          onChange={()=>{
+
+          }}
+        />
+        <input
+          type="text"
+          placeholder='enter Details'
+          className='px-5 py-2 border-2 h-40 rounded outline-none '
+          value={notes.descriptoin}
+          onChange={(e)=>{
+            console.log(e.target.value);
+            setNotes(notes.descriptoin);
+            
+          }}
+        />
+        <button 
+         className='bg-gray-400 px-5 py-2 text-black outline-none  '
+         >Add Note</button>
+      </form>
+    </div>
   )
 }
 
