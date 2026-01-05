3748b88795b56416f3b7e4c59c3d44d32e96d169
Gulshan kumar <3435tonighttillthelights@gmail.com>
2026-01-06 00:09:01 +0530

    redux started

---
+++ myapp17-Redux_basics/src/store.js
@@ -0,0 +1,7 @@
+import {configureStore} from "@reduxjs/toolkit"
+
+export const store = configureStore({
+    reducer:{
+        // here we will create slices -> like features for different things like for comments for follow for like 
+    },
+});
